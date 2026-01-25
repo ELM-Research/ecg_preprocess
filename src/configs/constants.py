cdf046787dea6525aad028f5efd984583c5dff4d
willxxy <90741489+willxxy@users.noreply.github.com>
2026-01-25 00:36:33 -0500

    Clean up constants in constants.py
    Removed unused pacing and pattern constants from the configuration.

--- src/configs/constants.py
+++ src/configs/constants.py
@@ -4,18 +4,3 @@ DATA_DIR = "../ECG-Bench/ecg_bench/data"
 LOG_DIR = "./.logs"
 
 PTB_ORDER = ["I", "II", "III", "aVL", "aVR", "aVF", "V1", "V2", "V3", "V4", "V5", "V6"]
-
-V_PACING_PATTERNS = ("v paced", "v-paced", "ventricular paced", 
-                     "ventricularly paced", "ventricular paced",
-                     "ventricular pacing", "ventricularly pacing",
-                     "v-pacing", "v pacing", "v pace", "v-pace",
-                     "ventricular pace", "ventricularly pace", )
-
-LBBB_PATTERNS = ("left bundle branch block", "lbbb",)
-
-QRS_DUR_WIDE_PATTERNS = ("wide",)
-
-### Name to Pattern Matching
-BATCH_LABEL_DICT = {"v_pacing": V_PACING_PATTERNS, 
-                   "lbbb": LBBB_PATTERNS, 
-                   "qrs_dur_wide": QRS_DUR_WIDE_PATTERNS}
